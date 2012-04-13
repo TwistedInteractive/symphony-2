9aeaa594086bac1150fb6c681056e255bc79eddf
Giel Berkers <info@gielberkers.com>
2012-04-13 17:22:49 +0200

    Added unique hash to fields edit screen

--- symphony/lib/toolkit/class.field.php
+++ symphony/lib/toolkit/class.field.php
@@ -441,6 +441,8 @@
 			// Create content
 			$wrapper->appendChild(Widget::Input('fields['.$this->get('sortorder').'][type]', $this->handle(), 'hidden'));
 			if($this->get('id')) $wrapper->appendChild(Widget::Input('fields['.$this->get('sortorder').'][id]', $this->get('id'), 'hidden'));
+			if($this->get('unique_hash')) $wrapper->appendChild(Widget::Input('fields['.$this->get('sortorder').'][unique_hash]',
+				$this->get('unique_hash'), 'hidden'));
 
 			$wrapper->appendChild($this->buildSummaryBlock($errors));
 		}
